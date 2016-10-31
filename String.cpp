7d9d3000b2ba4dba939eac5c4f72f6ba723221f1
FernandoCD <fercond002@gmail.com>
2016-10-30 19:30:21 -0500

    Add files via upload

---
+++ String.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+const string vowels = "aeiou";
+string pigLatinify(string s);
+
+int main(){
+	
+	string a = "outlast";
+	cout << pigLatinify(a);
+	
+	return 0;
+}
+
+string pigLatinify(string a){
+	if(a.find("qu") == 0)
+		return a.substr(2, a.size()-2) + a.substr(0, 2) + "ay";
+	else{
+		unsigned i = 0;
+		while(i < vowels.size()){
+			if(a[0] == vowels[i])
+				return a + "way";
+			i++;
+		}
+	}
+	return a.substr(1, a.size()-1) + a.substr(0, 1) + "ay";
+}
